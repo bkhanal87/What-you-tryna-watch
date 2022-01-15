91aba46f71c8a06c2ba1421e3008f7151fb4dba8
Christian Tanicala <christian.tanicala@gmail.com>
2022-01-15 11:54:28 -0500

    heroku deploy - console.logs

--- client/src/Components/SearchBar/searchMovie.js
+++ client/src/Components/SearchBar/searchMovie.js
@@ -12,21 +12,21 @@ const SearchBar = () => {
   const [options, setOptions] = useState([]);
   useEffect(() => {
     const movie = [];
-    console.log("running");
+    
     if (search.length > 3) {
       fetch(`https://www.omdbapi.com/?s=${search}&page=1&apikey=c58b06f8`)
         .then((response) => {
           if (!response.ok) {
-            console.log(`Did not get an ok. got: ${response.statusText}`);
+            
           }
-          console.log(response);
+          
           setDisplay(true);
           return response.json();
         })
         .then((json) => setOptions(json.Search))
         .catch((error) => {
           setDisplay(false);
-          console.log(`Error getting data: ${error.message}`);
+          
         });
     }
     // setOptions(movie);
@@ -37,17 +37,17 @@ const SearchBar = () => {
   }
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(search);
+    
     fetch(`https://www.omdbapi.com/?t=${search}&apikey=c58b06f8`)
       .then((response) => {
         if (!response.ok) {
-          console.log(`Did not get an ok. got: ${response.statusText}`);
+          
         }
         return response.json();
       })
       .then((json) => setMovie(json))
       .catch((error) => {
-        console.log(`Error getting data: ${error.message}`);
+        
       });
   }
   return (
